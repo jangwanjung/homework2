1a50cf8c058a23bf142eda5a5cfa7b55183c77cd
장완중 <tjdrj530@naver.com>
2025-05-22 15:36:36 +0900

    api명세서와 스키마생성

---
+++ schedule.sql
@@ -0,0 +1,10 @@
+create table schedule
+(
+    id bigint auto_increment primary key,
+    task text not null,
+    author_name varchar(100) not null,
+    password varchar(100) not null,
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp on update current_timestamp
+
+)
---
+++ src/test/java/org/example/homework2/Homework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.homework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
